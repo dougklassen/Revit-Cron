5231c9183551dbcd2c6414afe1bc22ad11bffe55
Doug Klassen <d.p.klassen@gmail.com>
2014-07-15 12:13:01 -0700

    added models and repos to rCron library

---
+++ RevitCron/RevitCron/Repositories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DougKlassen.Revit.Cron.Models;
+
+namespace DougKlassen.Revit.Cron.Repositories
+{
+    public interface IrCronTasksRepo
+    {
+        public rCronTask GetRCronTask();
+
+        public void PutRCronTask(rCronTask);
+    }
+}
