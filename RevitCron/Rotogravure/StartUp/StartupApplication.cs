cb4ba5ba4fc5e73aeb7be3f78994dda888c2b471
Doug Klassen <d.p.klassen@gmail.com>
2014-07-14 22:01:31 -0700

    added a solution and projects for Rotogravure and the class library

---
+++ RevitCron/Rotogravure/StartUp/StartupApplication.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Reflection;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using DougKlassen.Revit.Cron;
+
+namespace DougKlassen.Revit.Cron.Rotogravure.StartUp
+{
+    public static class FileLocations
+    {
+        //AddInDirectory is initialized at runtime
+        public static String AddInDirectory;
+        public static String AssemblyName;
+        public static readonly String imperialTemplateDirectory = @"C:\ProgramData\Autodesk\RVT 2014\Family Templates\English_I\";
+        public static readonly String ResourceNameSpace = @"Rotogravure.Resources";
+    }
+
+    public class StartUpApp : IExternalApplication
+    {
+        Result IExternalApplication.OnStartup(UIControlledApplication application)
+        {
+            //initialize AssemblyName using reflection
+            FileLocations.AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            //initialize AddInDirectory. The addin should be stored in a directory named after the assembly
+            FileLocations.AddInDirectory = application.ControlledApplication.AllUsersAddinsLocation + "\\" + FileLocations.AssemblyName + "\\";
+
+            String msg = String.Empty;
+            msg += "Rotogravure initialized\n";
+            msg += RevitCron.Init;
+
+            TaskDialog.Show("Startup", msg);
+
+            return Result.Succeeded;
+        }
+
+        Result IExternalApplication.OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
