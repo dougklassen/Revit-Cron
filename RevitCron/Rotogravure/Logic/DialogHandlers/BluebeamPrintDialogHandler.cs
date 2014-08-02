14cdfd0582efd8150a537273b329430ab6dbfd47
Doug Klassen <d.p.klassen@gmail.com>
2014-08-02 16:15:59 -0700

    reworked BluebeamPrintDialogHandler to move logic from EnumWindows callback to Save method

--- RevitCron/Rotogravure/Logic/DialogHandlers/BluebeamPrintDialogHandler.cs
+++ RevitCron/Rotogravure/Logic/DialogHandlers/BluebeamPrintDialogHandler.cs
@@ -1,108 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Timers;
-using System.Windows.Shapes;
+﻿using DougKlassen.Revit.Cron.Models;
 
-using DougKlassen.Revit.Cron.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
 
 namespace DougKlassen.Revit.Cron.Rotogravure.Logic
 {
-    public static class BluebeamPrintDialogHandler
-    {
-        private static RCronLog log = RCronLog.Instance;
-        private static String saveFilePath;
+	public static class BluebeamPrintDialogHandler
+	{
+		private static RCronLog log = RCronLog.Instance;
+		private static String saveFilePath;
+		private static AutoResetEvent taskProcessingWaitHandle;
+		private static IntPtr hWndSaveDialog = IntPtr.Zero;
 
-        private const String SaveDialogCaption = "Save As";
-        private const String SaveBtnText = "&Save";
-        private const Double WaitTime = 3000; //todo: read from RotogravureOptions
-        private static Timer waitTimer;
+		private const String SaveDialogCaption = "Save As";
+		private const String SaveBtnText = "&Save";
 
-        public static void Save(String filePath)
-        {
-            saveFilePath = filePath;
-            waitTimer = new Timer(WaitTime);
-            waitTimer.Elapsed += WaitTimerElapsedSaveAction;
-            log.AppendLine("Starting {0}MS timer", WaitTime);
-            waitTimer.Start();
-        }
+		public static void Save(AutoResetEvent handle, String filePath)
+		{
+			Thread currThrd = Thread.CurrentThread;
+			System.Windows.Forms.MessageBox.Show(String.Format(
+				"In Save method\nthread: {0}\nId: {1}\napplication context: {2}",
+				currThrd.Name,
+				currThrd.ManagedThreadId.ToString(),
+				AppDomain.CurrentDomain.FriendlyName));
 
-        private static void WaitTimerElapsedSaveAction(object sender, ElapsedEventArgs e)
-        {
-            waitTimer.Stop();
-            log.AppendLine("Timer elapsed");
+			taskProcessingWaitHandle = handle;
+			saveFilePath = filePath;
+			Thread.Sleep(3000); //give the window time to display
+			WinApi.User32.EnumWindows(EnumWindowsProcSaveAction, 0);
+			Stopwatch rt = new Stopwatch();
+			rt.Start();
+			TimeSpan timeOut = new TimeSpan(0, 0, 30);
+			while(IntPtr.Zero == hWndSaveDialog)
+			{
+				Thread.Sleep(500);
+				if (rt.Elapsed > timeOut)
+				{
+					throw new Exception("Couldn't find save dialog");
+				}
+			};
 
-            WinApi.User32.EnumWindows(EnumWindowsProcSaveAction, 0);
-        }
+			System.IntPtr hWndChild;
+			StringBuilder winTextSb = new StringBuilder(256);
+			String winText;
 
-        private static Boolean EnumWindowsProcSaveAction( IntPtr hWnd, Int32 lParam)
-        {
-            StringBuilder titleSb = new StringBuilder(256);
-            WinApi.User32.GetWindowText(hWnd, titleSb, titleSb.Capacity);
-            String title = titleSb.ToString();
-            log.AppendLine("Window: hWnd={0}, title=\"{1}\"", hWnd, title);
+			//set save location
+			List<IntPtr> hWndComboBoxes = new List<IntPtr>();
+			hWndChild = WinApi.User32.GetWindow(hWndSaveDialog, WinApi.User32.GW_CHILD);
+			do
+			{
+				WinApi.User32.GetClassName(hWndChild, winTextSb, winTextSb.Capacity);
+				winText = winTextSb.ToString();
+				if (winText.Equals("ComboBoxEx32"))
+				{
+					hWndComboBoxes.Add(hWndChild);
+				}
+				hWndChild = WinApi.User32.GetWindow(hWndChild, WinApi.User32.GW_HWNDNEXT);
+			} while (IntPtr.Zero != hWndChild);
+			IntPtr hWndFileName = IntPtr.Zero;
+			RECT rect = new RECT();
+			Int32 minRect = Int32.MaxValue;
+			for (int i = 0; i < hWndComboBoxes.Count; i++)
+			{
+				WinApi.User32.GetWindowRect(hWndComboBoxes[i], ref rect);
+				if (rect.Top < minRect)
+				{
+					hWndFileName = hWndComboBoxes[i];
+					minRect = rect.Top;
+				}
+			}
+			log.AppendLine("  --File Name textbox found: hWnd={0}");
+			log.AppendLine("    setting text to \"{0}\"", saveFilePath);
+			WinApi.User32.SendMessage(hWndFileName, WinApi.User32.WM_SETTEXT, 0, saveFilePath);
 
-            if (SaveDialogCaption == title)
-            {
-                log.AppendLine("--target window found");
-                System.IntPtr hWndChild;
-                StringBuilder winTextSb = new StringBuilder(256);
-                String winText;
+			//click Save As button
+			winTextSb = new StringBuilder(256);
+			hWndChild = WinApi.User32.GetWindow(hWndSaveDialog, WinApi.User32.GW_CHILD);
+			do
+			{
+				WinApi.User32.GetWindowText(hWndChild, winTextSb, winTextSb.Capacity);
+				winText = winTextSb.ToString();
+				if (SaveBtnText == winText)
+				{
+					log.AppendLine("  --Save button found: hWnd={0}, title=\"{1}\"", hWndChild, winText);
+					log.AppendLine("    clicking button");
+					WinApi.User32.SendMessage(hWndChild, WinApi.User32.BM_SETSTATE, 1, 0);
+					WinApi.User32.SendMessage(hWndChild, WinApi.User32.WM_LBUTTONDOWN, 0, 0);
+					WinApi.User32.SendMessage(hWndChild, WinApi.User32.WM_LBUTTONUP, 0, 0);
+					WinApi.User32.SendMessage(hWndChild, WinApi.User32.BM_SETSTATE, 1, 0);
+					System.Windows.Forms.MessageBox.Show("Releasing TaskProcessing");
+					taskProcessingWaitHandle.Set(); //allow TaskProcessingLogic to continue with other tasks
+				}
+				hWndChild = WinApi.User32.GetWindow(hWndChild, WinApi.User32.GW_HWNDNEXT);
+			} while (IntPtr.Zero != hWndChild);
+		}
 
-                //set save location
-                List<IntPtr> hWndComboBoxes = new List<IntPtr>();
-                hWndChild = WinApi.User32.GetWindow(hWnd, WinApi.User32.GW_CHILD);
-                do
-                {
-                    WinApi.User32.GetClassName(hWndChild, winTextSb, winTextSb.Capacity);
-                    winText = winTextSb.ToString();
-                    if (winText.Equals("ComboBoxEx32"))
-                    {
-                        hWndComboBoxes.Add(hWndChild);
-                    }
-                    hWndChild = WinApi.User32.GetWindow(hWndChild, WinApi.User32.GW_HWNDNEXT);
-                } while (IntPtr.Zero != hWndChild);
-                IntPtr hWndFileName = IntPtr.Zero;
-                RECT rect = new RECT();
-                Int32 minRect = Int32.MaxValue;
-                for (int i = 0; i < hWndComboBoxes.Count; i++)
-			    {
-                    WinApi.User32.GetWindowRect(hWndComboBoxes[i], ref rect);
-                    if (rect.Top < minRect)
-                    {
-                        hWndFileName = hWndComboBoxes[i];
-                        minRect = rect.Top;
-                    }
-			    }
-                log.AppendLine("  --File Name textbox found: hWnd={0}");
-                log.AppendLine("    setting text to \"{0}\"", saveFilePath);
-                WinApi.User32.SendMessage(hWndFileName, WinApi.User32.WM_SETTEXT, 0, saveFilePath);
+		private static Boolean EnumWindowsProcSaveAction(IntPtr hWnd, Int32 lParam)
+		{
+			StringBuilder titleSb = new StringBuilder(256);
+			WinApi.User32.GetWindowText(hWnd, titleSb, titleSb.Capacity);
+			String title = titleSb.ToString();
+			log.AppendLine("Window: hWnd={0}, title=\"{1}\"", hWnd, title);
 
-                //click Save As button
-                winTextSb = new StringBuilder(256);
-                hWndChild = WinApi.User32.GetWindow(hWnd, WinApi.User32.GW_CHILD);
-                do
-                {
-                    WinApi.User32.GetWindowText(hWndChild, winTextSb, winTextSb.Capacity);
-                    winText = winTextSb.ToString();
-                    if (SaveBtnText == winText)
-                    {
-                        log.AppendLine("  --Save button found: hWnd={0}, title=\"{1}\"", hWndChild, winText);
-                        log.AppendLine("    clicking button");
-                        WinApi.User32.SendMessage(hWndChild, WinApi.User32.BM_SETSTATE, 1, 0);
-                        WinApi.User32.SendMessage(hWndChild, WinApi.User32.WM_LBUTTONDOWN, 0, 0);
-                        WinApi.User32.SendMessage(hWndChild, WinApi.User32.WM_LBUTTONUP, 0, 0);
-                        WinApi.User32.SendMessage(hWndChild, WinApi.User32.BM_SETSTATE, 1, 0);
-                        return false;    //since the button was found, stop enumeration of child windows
-                    }
-                    hWndChild = WinApi.User32.GetWindow(hWndChild, WinApi.User32.GW_HWNDNEXT);
-                } while (IntPtr.Zero != hWndChild);
-            }
+			if (SaveDialogCaption == title)
+			{
+				Thread currThrd = Thread.CurrentThread;
+				System.Windows.Forms.MessageBox.Show(String.Format(
+					"In EnumWindows callback\nthread: {0}\nId: {1}\napplication context: {2}",
+					currThrd.Name,
+					currThrd.ManagedThreadId.ToString(),
+					AppDomain.CurrentDomain.FriendlyName));
+				log.AppendLine("--target window found");
+				hWndSaveDialog = hWnd;
+				return false;
+			}
 
-            return true;    //continue enumeration of child windows
-        }
+			return true;
+		}
 
-        //todo: prevent Bluebeam from opening
-    }
+		//todo: prevent Bluebeam from opening
+	}
 }
--- RevitCron/Rotogravure/Logic/TaskProcessingLogic.cs
+++ RevitCron/Rotogravure/Logic/TaskProcessingLogic.cs
@@ -1,158 +1,174 @@
-﻿using System;
+﻿using Autodesk.Revit.DB;
+
+using DougKlassen.Revit.Cron.Models;
+using DougKlassen.Revit.Cron.Repositories;
+using DougKlassen.Revit.Cron.Rotogravure.Interface;
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-
-using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
-
-using DougKlassen.Revit.Cron;
-using DougKlassen.Revit.Cron.Models;
-using DougKlassen.Revit.Cron.Repositories;
-using DougKlassen.Revit.Cron.Rotogravure;
-using DougKlassen.Revit.Cron.Rotogravure.Interface;
+using System.Threading;
 
 namespace DougKlassen.Revit.Cron.Rotogravure.Logic
 {
-    public static class TaskProcessingLogic
-    {
-        private static RotogravureOptions options;
-        private static RCronLog log;
+	public static class TaskProcessingLogic
+	{
+		private static RotogravureOptions options;
+		private static RCronLog log;
 
-        public static void OnApplicationInitialized(object sender, Autodesk.Revit.DB.Events.ApplicationInitializedEventArgs e)
-        {
-            {
-                log = RCronLog.Instance;
-                try
-                {
-                    options = RotogravureOptionsJsonRepo.LoadOptions(new Uri(RCronFileLocations.OptionsFilePath));
-                }
-                catch(Exception exc)
-                {
-                    options = new RotogravureOptions();
-                    log.LogException(exc);
-                }
-            }            
+		public static void OnApplicationInitialized(object sender, Autodesk.Revit.DB.Events.ApplicationInitializedEventArgs e)
+		{
+			{
+				log = RCronLog.Instance;
+				try
+				{
+					options = RotogravureOptionsJsonRepo.LoadOptions(new Uri(RCronFileLocations.OptionsFilePath));
+				}
+				catch (Exception exc)
+				{
+					options = new RotogravureOptions();
+					log.LogException(exc);
+				}
+			}
 
-            AssemblyName asm = Assembly.GetExecutingAssembly().GetName();
-            log.AppendLine("+++ {0} Rotogravure initialized", DateTime.Now);
-            log.AppendLine("assembly: {0}", asm.Name);
-            log.AppendLine("version: {0}", asm.Version.ToString());
+			AssemblyName asm = Assembly.GetExecutingAssembly().GetName();
+			log.AppendLine("+++ {0} Rotogravure initialized", DateTime.Now);
+			log.AppendLine("assembly: {0}", asm.Name);
+			log.AppendLine("version: {0}", asm.Version.ToString());
 
-            ICollection<RCronTask> tasks;
-            try
-            {
-                tasks = RCronTasksJsonRepo.LoadTasks(options.TasksFileUri);
-            }
-            catch (Exception exception)
-            {
-                log.LogException(exception);
-                LogWindow.Show(log);
-                throw exception; //can't continue if the TasksRepo can't be loaded
-            }
+			ICollection<RCronTask> tasks;
+			try
+			{
+				tasks = RCronTasksJsonRepo.LoadTasks(options.TasksFileUri);
+			}
+			catch (Exception exception)
+			{
+				log.LogException(exception);
+				LogWindow.Show(log);
+				throw exception; //can't continue if the TasksRepo can't be loaded
+			}
 
-            log.AppendLine("+++ {0} tasks found", tasks.Count);
+			log.AppendLine("+++ {0} tasks found", tasks.Count);
 
-            Autodesk.Revit.ApplicationServices.Application app = (Autodesk.Revit.ApplicationServices.Application)sender;
-            Document dbDoc;
-            Boolean saveModified = false;
+			Autodesk.Revit.ApplicationServices.Application app = (Autodesk.Revit.ApplicationServices.Application)sender;
+			Document dbDoc;
+			Boolean saveModified = false;
 
-            foreach (RCronTask task in tasks)
-            {
-                try
-                {
-                    dbDoc = app.OpenDocumentFile(task.TaskInfo.ProjectFile);
-                    log.AppendLine("---");
+			foreach (RCronTask task in tasks)
+			{
+				try
+				{
+					dbDoc = app.OpenDocumentFile(task.TaskInfo.ProjectFile);
+					log.AppendLine("---");
 
-                    switch (task.TaskInfo.TaskType)
-                    {
-                        case TaskType.Print:
-                            log.AppendLine("print task: {0}", dbDoc.PathName);
-                            RCronPrintTaskInfo printTaskInfo = (RCronPrintTaskInfo)task.TaskInfo;
-                            ViewSheetSet printSet = new FilteredElementCollector(dbDoc)
-                                .OfClass(typeof(ViewSheetSet))
-                                .Where(s => s.Name.Equals(printTaskInfo.PrintSet))
-                                .FirstOrDefault() as ViewSheetSet;
+					switch (task.TaskInfo.TaskType)
+					{
+						case TaskType.Print:
+							log.AppendLine("print task: {0}", dbDoc.PathName);
+							RCronPrintTaskInfo printTaskInfo = (RCronPrintTaskInfo)task.TaskInfo;
+							ViewSheetSet printSet = new FilteredElementCollector(dbDoc)
+									.OfClass(typeof(ViewSheetSet))
+									.Where(s => s.Name.Equals(printTaskInfo.PrintSet))
+									.FirstOrDefault() as ViewSheetSet;
 
-                            if (null != printSet)
-                            {
-                                log.AppendLine("--printing {0} views", printSet.Views.Size.ToString());
-                                foreach (View v in printSet.Views)
-                                {
-                                    log.AppendLine("  view: \"{0}\"", v.Name);
-                                }
+							if (null != printSet)
+							{
+								log.AppendLine("--printing {0} views", printSet.Views.Size.ToString());
+								foreach (View v in printSet.Views)
+								{
+									log.AppendLine("  view: \"{0}\"", v.Name);
+								}
 
-                                PrintManager pm = dbDoc.PrintManager;
-                                pm.SelectNewPrintDriver("Bluebeam PDF"); //todo: read from RotogravureOptions
-                                pm.PrintSetup
-                                    .InSession
-                                    .PrintParameters
-                                    .PaperSize = pm.PaperSizes
-                                        .Cast<PaperSize>()
-                                        .Where(p => "Letter" == p.Name) //todo: read from RotogravureOptions
-                                        .FirstOrDefault();
-                                pm.PrintRange = PrintRange.Select;
-                                pm.ViewSheetSetting.CurrentViewSheetSet = printSet;
-                                pm.CombinedFile = true;
-                                pm.SubmitPrint();
+								PrintManager pm = dbDoc.PrintManager;
+								pm.SelectNewPrintDriver("Bluebeam PDF"); //todo: read from RotogravureOptions
+								pm.PrintSetup
+										.InSession
+										.PrintParameters
+										.PaperSize = pm.PaperSizes
+												.Cast<PaperSize>()
+												.Where(p => "Letter" == p.Name) //todo: read from RotogravureOptions
+												.FirstOrDefault();
+								pm.PrintRange = PrintRange.Select;
+								pm.ViewSheetSetting.CurrentViewSheetSet = printSet;
+								pm.CombinedFile = true;
+								pm.SubmitPrint();
 
-                                String outputDirectoryPath = printTaskInfo.OutputDirectory + RCronCanon.TimeStamp + '\\';
-                                if (!Directory.Exists(outputDirectoryPath))
-                                {
-                                    Directory.CreateDirectory(outputDirectoryPath);
-                                }
-                                else
-                                {
-                                    if (File.Exists(outputDirectoryPath + printTaskInfo.OutputFileName)) //if the filename already exists, append an auto-incremented suffix to it
-                                    {   //todo: move this stuff to RCronCanon
-                                        Int32 version, highestVersion = 0;
-                                        Regex suffixRegex = new Regex(@"(?<=\.)\d\d\d$");
-                                        foreach (String f in Directory.GetFiles(outputDirectoryPath))
-                                        {
-                                            Match suffixMatch = suffixRegex.Match(f);
-                                            if (suffixMatch.Success)
-                                            {
-                                                version = Int32.Parse(suffixMatch.Value);
-                                                highestVersion = version > highestVersion ? version : highestVersion;
-                                            }
-                                        }
-                                        highestVersion++;
-                                        printTaskInfo.OutputFileName = suffixRegex.Replace(
-                                            printTaskInfo.OutputFileName,
-                                            String.Format("{0:D3}", highestVersion));                                        
-                                    }
-                                }
-                                BluebeamPrintDialogHandler.Save(outputDirectoryPath + printTaskInfo.OutputFileName);
-                            }
-                            else
-                            {
-                                log.AppendLine("error: couldn't load printset {0}", printTaskInfo.PrintSet);
-                            }
+								String outputDirectoryPath = printTaskInfo.OutputDirectory + RCronCanon.TimeStamp + '\\';
+								if (!Directory.Exists(outputDirectoryPath))
+								{
+									Directory.CreateDirectory(outputDirectoryPath);
+								}
+								else
+								{
+									if (File.Exists(outputDirectoryPath + printTaskInfo.OutputFileName)) //if the filename already exists, append an auto-incremented suffix to it
+									{   //todo: move this stuff to RCronCanon
+										Int32 version, highestVersion = 0;
+										Regex suffixRegex = new Regex(@"(?<=\.)\d\d\d$");
+										foreach (String f in Directory.GetFiles(outputDirectoryPath))
+										{
+											Match suffixMatch = suffixRegex.Match(f);
+											if (suffixMatch.Success)
+											{
+												version = Int32.Parse(suffixMatch.Value);
+												highestVersion = version > highestVersion ? version : highestVersion;
+											}
+										}
+										highestVersion++;
+										printTaskInfo.OutputFileName = suffixRegex.Replace(
+												printTaskInfo.OutputFileName,
+												String.Format("{0:D3}", highestVersion));
+									}
+								}
 
-                            break;
-                        case TaskType.Export:
-                            log.AppendLine("export task: {0}", dbDoc.PathName);
-                            break;
-                        case TaskType.ETransmit:
-                            log.AppendLine("eTransmit task: {0}", dbDoc.PathName);
-                            break;
-                        case TaskType.Command:
-                            log.AppendLine("command task: {0}", dbDoc.PathName);
-                            break;
-                        default:
-                            break;
-                    }
+								Thread.CurrentThread.Name = "RotoGravure thread";
+								AutoResetEvent externalTaskWaitHandle = new AutoResetEvent(false);
+								BluebeamPrintDialogHandler.Save(
+									externalTaskWaitHandle,
+									outputDirectoryPath + printTaskInfo.OutputFileName);
+								////launch the dialog handler in a new thread and wait until it completes work
+								//AutoResetEvent externalTaskWaitHandle = new AutoResetEvent(false);
+								//ThreadStart ts = new ThreadStart(() =>
+								//	{
+								//		BluebeamPrintDialogHandler.Save(
+								//				externalTaskWaitHandle,
+								//				outputDirectoryPath + printTaskInfo.OutputFileName);
+								//	});
+								//Thread dialogHandlerThread = new Thread(ts)
+								//	{
+								//		Name = "BluebeamDialogHandler"
+								//	};
+								//dialogHandlerThread.Start();
+								////externalTaskWaitHandle.WaitOne();
+							}
+							else
+							{
+								log.AppendLine("error: couldn't load printset {0}", printTaskInfo.PrintSet);
+							}
 
-                    dbDoc.Close(saveModified);
-                }
-                catch (Exception exception)
-                {
-                    log.LogException(exception);
-                }
-            }
-        }
-    }
+							break;
+						case TaskType.Export:
+							log.AppendLine("export task: {0}", dbDoc.PathName);
+							break;
+						case TaskType.ETransmit:
+							log.AppendLine("eTransmit task: {0}", dbDoc.PathName);
+							break;
+						case TaskType.Command:
+							log.AppendLine("command task: {0}", dbDoc.PathName);
+							break;
+						default:
+							break;
+					}
+
+					dbDoc.Close(saveModified);
+				}
+				catch (Exception exception)
+				{
+					log.LogException(exception);
+				}
+			}
+		}
+	}
 }
