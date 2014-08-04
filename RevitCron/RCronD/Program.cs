adc1682bb21ead7b5080f08b8f22074231e4e0be
Doug Klassen <d.p.klassen@gmail.com>
2014-08-04 07:08:58 -0700

    added project, service installer

--- RevitCron/RCronD/Program.cs
+++ RevitCron/RCronD/Program.cs
@@ -5,31 +5,31 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DougKlassen.Revit.Cron.RCronD
+namespace DougKlassen.Revit.Cron.Daemon
 {
-    class RCronService : System.ServiceProcess.ServiceBase
-    {
-        public RCronService()
-        {
-            this.ServiceName = "RCronD";
-            this.CanStop = true;
-            this.CanPauseAndContinue = true;
-            this.AutoLog = true;
-        }
+	public class RCronD : System.ServiceProcess.ServiceBase
+	{
+		public RCronD()
+		{
+			this.ServiceName = "RCronD";
+			this.CanStop = true;
+			this.CanPauseAndContinue = true;
+			this.AutoLog = true;
+		}
 
-        public static void Main()
-        {
-            System.ServiceProcess.ServiceBase.Run(new RCronService());
-        }
+		public static void Main()
+		{
+			System.ServiceProcess.ServiceBase.Run(new RCronD());
+		}
 
-        protected override void OnStart(string[] args)
-        {
-            base.OnStart(args);
-        }
+		protected override void OnStart(string[] args)
+		{
+			base.OnStart(args);
+		}
 
-        protected override void OnStop()
-        {
-            base.OnStop();
-        }
-    }
+		protected override void OnStop()
+		{
+			base.OnStop();
+		}
+	}
 }
