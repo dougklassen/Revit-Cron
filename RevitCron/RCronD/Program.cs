2cabc6e1d3685e83ce2f957c5305dd307d3dd220
Doug Klassen <d.p.klassen@gmail.com>
2014-08-05 16:43:14 -0700

    added icon resources and code file for RCronD

---
+++ RevitCron/RCronD/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace DougKlassen.Revit.Cron.Daemon
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ApplicationContext appContext = new RCronDAppContext();
+			Application.Run(appContext);
+		}
+	}
+}
