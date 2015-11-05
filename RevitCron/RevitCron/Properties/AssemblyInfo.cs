cd4ad05c3b48d0e7fad3418ff284469e28516bb5
Doug Klassen <d.p.klassen@gmail.com>
2015-11-05 08:49:01 -0800

    updates for Revit 2016

--- RevitCron/RevitCron/Properties/AssemblyInfo.cs
+++ RevitCron/RevitCron/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RevitCron")]
-[assembly: AssemblyCopyright("Copyright © Doug Klassen 2014")]
+[assembly: AssemblyCopyright("Copyright © Doug Klassen 2014-15")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("15.0.*")]
-[assembly: AssemblyFileVersionAttribute("15.0")]
+[assembly: AssemblyVersion("16.0.*")]
+[assembly: AssemblyFileVersionAttribute("16.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
